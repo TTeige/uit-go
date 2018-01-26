1f0a1bc87df24d43a7d0863ebf4b23e2cfa81398
TTeige <tim.teige91@gmail.com>
2018-01-26 22:15:27 +0100

    format file

--- autoscalingService/autoscalingService.go
+++ autoscalingService/autoscalingService.go
@@ -14,11 +14,11 @@ type Env struct {
 }
 
 func indexHandle(env *Env) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		jobs, err := models.AllJobs(env.db)
 		if err != nil {
 			log.Println(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
 		for _, job := range jobs {
@@ -29,53 +29,38 @@ func indexHandle(env *Env) http.Handler {
 	// Create serving for the overview of the algorithms that have been input to the scaling server
 }
 
-func submitJobHistoryHandle(env *Env) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-
-	})
-	// Submit metapipe history to the server, store the history and convert it to the appropriate format
-}
-
 func runScalingHandle(env *Env) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
 	// Run the autoscaling algorithm
-	// route the request to a list of whitelisted addresses. This should enable algorithms implemented in any language
-	// wait for response and write to database
 	// Request contains the current queue
 	// Do not create 201 response immediately and return expected output URI because of RFC 7231, section 6.3.2
-	// Use 202 Accepted and return expected output location /history/autoscaling/{id}
-
-	// If the request is to the same scaling algorithm, maybe create a queue to hold them instead.
+	// Use 202 Accepted and return expected output location autoscale/{id}
 }
 
 func getPreviousScalingHandle(env *Env) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 	})
-	// Return previous values for a simulated run
-
 	// Request for resource which has not yet completed, no entry in database, return (303 See Other) code, with a
-	// Location field in the header for /history/autoscaling/{id}
+	// Location field in the header for /autoscale/{id}
 	// If error for the autoscaling, return appropriate error code
 }
 
-func Serve(hostUrl string) {
+func Run(hostUrl string, db *sql.DB) error {
+	serve(hostUrl, db)
+	return nil
+}
 
-	db, err := models.NewDatabase("user=tim dbname=autoscaling password=something")
-	if err != nil {
-		log.Fatalf("%s", err)
-		return
-	}
-	env := &Env{db:db}
+func serve(hostUrl string, db *sql.DB) {
+
+	env := &Env{db: db}
 
 	r := mux.NewRouter()
 	r.Path("/").Methods("GET").Handler(indexHandle(env))
 	r.Path("/autoscale/").Methods("POST").Handler(runScalingHandle(env))
-	hist := r.PathPrefix("/history").Subrouter()
-	hist.Path("/jobs/").Methods("POST").Handler(submitJobHistoryHandle(env))
-	hist.Path("/autoscaling/{id}/").Methods("GET").Handler(getPreviousScalingHandle(env))
+	r.Path("/autoscale/{id}/").Methods("GET").Handler(getPreviousScalingHandle(env))
 
 	http.ListenAndServe(hostUrl, r)
 }
