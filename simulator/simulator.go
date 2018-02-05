8df3780548c4496a6d7184a97bf8c76ba376b8ae
TTeige <tim.teige91@gmail.com>
2018-02-05 20:45:44 +0100

    Added a dummy algorithm test

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -5,13 +5,15 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"github.com/tteige/uit-go/autoscale"
+	"time"
+	"log"
 )
 
 type Simulator struct {
 	DB        *sql.DB
 	Hostname  string
 	SimClouds []autoscale.Cloud
-	Algorithm autoscale.AlgorithmInterface
+	Algorithm autoscale.Algorithm
 }
 
 func (sim *Simulator) Run() {
@@ -32,10 +34,52 @@ func (sim *Simulator) getPreviousScalingHandle(w http.ResponseWriter, r *http.Re
 }
 
 func (sim *Simulator) simulationHandle(w http.ResponseWriter, r *http.Request) {
-	//An empty body request to this endpoint will initiate the simulation based on the inputted algorithm
-	//qh := autoscale.QueueHandle{
-	//	DB: sim.DB,
-	//}
+	// An empty body request to this endpoint will initiate the simulation based on the inputted algorithm
+	// A body with jobs: {...} uses these jobs, else, standard jobs are used
+	// A body with cloudState: {...} uses this cloud state, else, standard state is used
+
+	jobs := []autoscale.AlgorithmJob{
+		{
+			Id:                   "123abc",
+			Tags:                 []string{"aws", "csc"},
+			Parameters:           []string{"blast"},
+			State:                "RUNNING",
+			StateTransitionTimes: nil,
+			Deadline:             time.Now().Add(time.Hour),
+			Priority:             10,
+		},
+	}
+
+	clusters := []autoscale.Cluster{
+		{
+			Name:            "aws",
+			Limit:           100,
+			AcceptTags:      []string{"aws", "aws-meta"},
+			Types:           nil,
+			ActiveInstances: nil,
+		},
+		{
+			Name:            "cpouta",
+			Limit:           1000,
+			AcceptTags:      []string{"cpouta", "csc"},
+			Types:           nil,
+			ActiveInstances: nil,
+		},
+	}
+
+	algInput := autoscale.AlgorithmInput{
+		JobQueue: jobs,
+		Clusters: clusters,
+	}
+
+	out, err := sim.Algorithm.Step(algInput, time.Now().Add(time.Hour*2))
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("%+v", out)
 
 }
 
