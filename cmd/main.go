20107d99a671a6007d14ba66a76428e4a1a2ed2e
TTeige <tim.teige91@gmail.com>
2018-02-16 13:20:29 +0100

    Added config

--- cmd/main.go
+++ cmd/main.go
@@ -7,37 +7,51 @@ import (
 	"github.com/tteige/uit-go/models"
 	"github.com/tteige/uit-go/simulator"
 	"github.com/tteige/uit-go/algorithm"
+	"os"
+	"github.com/tteige/uit-go/config"
 )
 
 func main() {
 
 	service := flag.Bool("production", false, "run the auto scaling service")
-	dbHost := flag.String("databaseUrl", "user=tim dbname=autoscaling password=something", "database source")
-	hostname := flag.String("hostname", "localhost:8080", "hostname for either service or simulator")
 
-	db, err := models.OpenDatabase(*dbHost)
+	conf := config.FullConfig{}
+	err := conf.LoadConfig()
 	if err != nil {
-		log.Fatalf("%s", err)
+		log.Fatal(err)
+		return
+	}
+	serviceHostname := conf.ServiceConfig.Hostname + ":" + conf.ServiceConfig.Port
+
+	db, err := models.OpenDatabase(conf.DBConfig.User, conf.DBConfig.Host, conf.DBConfig.Password)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = models.InitDatabase(db)
+	if err != nil {
+		log.Fatal(err)
 		return
 	}
 
 	if *service {
-		log.Printf("Starting the auto scaling service at: %s ", *hostname)
+		log.Printf("Starting the auto scaling service at: %s ", serviceHostname)
 		s := autoscalingService.Service{
-			DB:db,
-			Hostname:*hostname,
+			DB:       db,
+			Hostname: serviceHostname,
 		}
 		s.Run()
 	} else {
-		log.Printf("Starting the auto scaling simulator at: %s ", *hostname)
 		alg := algorithm.NaiveAlgorithm{
-			ScaleUpThreshold: 10,
+			ScaleUpThreshold:   10,
 			ScaleDownThreshold: 3,
 		}
 		sim := simulator.Simulator{
-			DB:db,
-			Hostname:*hostname,
+			DB:        db,
+			Hostname:  serviceHostname,
 			Algorithm: alg,
+			Log:       log.New(os.Stdout, "SIMULATOR LOGGER: ", log.Lshortfile|log.LstdFlags),
 		}
 		sim.Run()
 	}
