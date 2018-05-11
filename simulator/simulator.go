e78e5b61269aacdd37b547991d8a52803db7ef83
TTeige <tim.teige91@gmail.com>
2018-05-11 15:19:22 +0200

    Added endpoint for creating instance types stored in the database. Added scaling event handling. Added form parsing for the name of the simulation

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -8,12 +8,16 @@ import (
 	"time"
 	"log"
 	"html/template"
+	"github.com/tteige/uit-go/models"
+	"strconv"
+	"github.com/segmentio/ksuid"
+	"strings"
 )
 
 type Simulator struct {
 	DB        *sql.DB
 	Hostname  string
-	SimClouds []autoscale.Cloud
+	SimClouds map[string]autoscale.Cloud
 	Algorithm autoscale.Algorithm
 	Log       *log.Logger
 	templates *template.Template
@@ -32,12 +36,40 @@ func (sim *Simulator) serveSim() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", sim.indexHandle).Methods("GET")
 	r.HandleFunc("/simulate/", sim.simulationHandle).Methods("POST")
+	r.HandleFunc("/simulate/instancetype/", sim.simulationHandle).Methods("POST")
 	r.HandleFunc("/simulate/{id}/", sim.getPreviousScalingHandle).Methods("GET")
 	http.ListenAndServe(sim.Hostname, r)
 }
 
 func (sim *Simulator) getPreviousScalingHandle(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func (sim *Simulator) addInstanceType(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	increment, err := strconv.Atoi(r.PostForm.Get("price_increment"))
+	if err != nil {
+		sim.Log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	iType := autoscale.InstanceType{Name: r.PostForm.Get("name"), PriceIncrement: increment}
+
+	clusterName := r.PostForm.Get("cluster_name")
+
+	err = models.InsertInstanceType(sim.DB, iType, clusterName)
+	if err != nil {
+		sim.Log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusCreated)
+	return
 }
 
 func (sim *Simulator) simulationHandle(w http.ResponseWriter, r *http.Request) {
@@ -59,36 +91,77 @@ func (sim *Simulator) simulationHandle(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	clusters := []autoscale.Cluster{
-		{
-			Name:            "aws",
-			Limit:           100,
-			AcceptTags:      []string{"aws", "aws-meta"},
-			Types:           nil,
-		},
-		{
-			Name:            "cpouta",
-			Limit:           1000,
-			AcceptTags:      []string{"cpouta", "csc"},
-			Types:           nil,
-		},
+	//clusters := []autoscale.Cluster{
+	//	{
+	//		Name:       "aws",
+	//		Limit:      100,
+	//		AcceptTags: []string{"aws", "aws-meta"},
+	//		Types:      nil,
+	//	},
+	//	{
+	//		Name:       "cpouta",
+	//		Limit:      1000,
+	//		AcceptTags: []string{"cpouta", "csc"},
+	//		Types:      nil,
+	//	},
+	//}
+
+	err := r.ParseForm()
+	if err != nil {
+		sim.Log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	algInput := autoscale.AlgorithmInput{
-		JobQueue: jobs,
-		Clusters: clusters,
+
+	clust, err := models.GetDefaultClusters(sim.DB)
+	if err != nil {
+		sim.Log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	startTime := time.Now()
+	var algInput autoscale.AlgorithmInput
+	algInput.JobQueue = jobs
+	algInput.Clusters = clust
 
+	SimStartTime := time.Now()
+	friendlyName := r.PostForm.Get("name")
+
+	if friendlyName == "" {
+		friendlyName = ksuid.New().String()
+	}
+
+
+	simHandle, err := models.CreateSimulation(sim.DB, friendlyName, SimStartTime)
+	if err != nil {
+		sim.Log.Print(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	algStartTime := time.Now()
 	for i := 0; i < 10; i++ {
-		out, err := sim.Algorithm.Step(algInput, startTime.Add(time.Minute * time.Duration(30 * i)))
+		out, err := sim.Algorithm.Step(algInput, algStartTime.Add(time.Minute*time.Duration(30*i)))
 		if err != nil {
 			sim.Log.Print(err)
-			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		sim.Log.Printf("%+v", out)
+
+		for _, e := range out.Instances {
+			for key, cluster := range sim.SimClouds {
+				if strings.Contains(strings.ToLower(e.ClusterTag), strings.ToLower(key)) {
+					err = cluster.ProcessEvent(e, simHandle.Name)
+					if err != nil {
+						sim.Log.Print(err)
+						http.Error(w, err.Error(), http.StatusInternalServerError)
+						return
+					}
+				}
+			}
+		}
+		algInput.JobQueue = out.JobQueue
 	}
 
 }
