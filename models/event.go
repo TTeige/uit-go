16338b94d9534ecb2e74dc4e3683282220f167b3
TTeige <tim.teige91@gmail.com>
2018-05-19 17:10:51 +0200

    Updated sim event

--- models/event.go
+++ models/event.go
@@ -3,14 +3,16 @@ package models
 import (
 	"database/sql"
 	"time"
+	"github.com/tteige/uit-go/autoscale"
 )
 
 type SimEvent struct {
-	Id         int
-	SimId      string
-	Created    time.Time
-	InstanceId string
-	Type       string
+	Id           int
+	SimId        string
+	Created      time.Time
+	Instance     autoscale.Instance
+	InstanceType autoscale.InstanceType
+	Type         string
 }
 
 func GetSimEvent(db *sql.DB, id string) (*SimEvent, error) {
@@ -23,8 +25,9 @@ func GetSimEvent(db *sql.DB, id string) (*SimEvent, error) {
 }
 
 func WriteSimEvent(db *sql.DB, event SimEvent) error {
-	_, err := db.Exec("INSERT INTO sim_events (sim_name, created, instance_id, type) VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING",
-		event.SimId, event.Created, event.InstanceId, event.Type)
+	_, err := db.Exec("INSERT INTO sim_events (sim_name, created, instance_id, type, instance_type, price, instance_state)"+
+		"VALUES ($1, $2, $3, $4, $5, $6, $7)ON CONFLICT DO NOTHING",
+		event.SimId, event.Created, event.Instance.Id, event.Type, event.Instance.Type, event.InstanceType.PriceIncrement, event.Instance.State)
 	if err != nil {
 		return err
 	}
