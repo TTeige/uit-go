80219ea8629a44c8dc525e33138a63f5bcd417b7
TTeige <tim.teige91@gmail.com>
2018-05-27 11:49:25 +0200

    fixed bugs

--- models/algorithm_job.go
+++ models/algorithm_job.go
@@ -6,8 +6,8 @@ import (
 )
 
 func InsertAlgorithmJob(db *sql.DB, job autoscale.AlgorithmJob, runName string) error {
-	_, err := db.Exec("INSERT INTO algorithm_job (run_name, jobid, created, started, executiontime, tag, deadline, priority, state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-		runName, job.Id, job.Created, job.Started, job.ExecutionTime, job.Tag, job.Deadline, job.Priority, job.State)
+	_, err := db.Exec("INSERT INTO algorithm_job (run_name, jobid, created, started, executiontime, tag, deadline, priority, state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+		runName, job.Id, job.Created, job.Started, job.ExecutionTime[job.Tag], job.Tag, job.Deadline, job.Priority, job.State)
 	if err != nil {
 		return err
 	}
@@ -15,17 +15,22 @@ func InsertAlgorithmJob(db *sql.DB, job autoscale.AlgorithmJob, runName string) 
 }
 
 func GetAllAlgorithmJobs(db *sql.DB, runName string) ([]autoscale.AlgorithmJob, error) {
-	rows, err := db.Query("SELECT (jobid, created, started, executiontime, tag, deadline, priority, state) FROM algorithm_job WHERE run_name = $1", runName)
+	rows, err := db.Query("SELECT * FROM algorithm_job WHERE run_name = $1", runName)
 	if err != nil {
 		return nil, err
 	}
 	var jobs []autoscale.AlgorithmJob
 	for rows.Next() {
 		var job autoscale.AlgorithmJob
-		err = rows.Scan(&job.Id, &job.Created, &job.Started, &job.ExecutionTime, &job.Tag, &job.Deadline, &job.Priority, &job.State)
+		var execTime int64
+		var id int
+		var dbRunName string
+		err = rows.Scan(&id, &dbRunName, &job.Id, &job.Created, &job.Started, &execTime, &job.Tag, &job.Deadline, &job.Priority, &job.State)
 		if err != nil {
 			return nil, err
 		}
+		job.ExecutionTime = make(map[string]int64)
+		job.ExecutionTime[job.Tag] = execTime
 		jobs = append(jobs, job)
 	}
 	return jobs, nil
--- models/simulator_event.go
+++ models/simulator_event.go
@@ -9,11 +9,12 @@ type SimulatorEvent struct {
 	RunName            string
 	QueueDuration      int64
 	AlgorithmTimestamp time.Time
+	Tag                string
 }
 
 func InsertSimulatorEvent(db *sql.DB, event SimulatorEvent) error {
-	_, err := db.Exec("INSERT INTO simulator_events (run_name, queue_duration, alg_timestamp) VALUES ($1, $2, $3)",
-		event.RunName, event.QueueDuration, event.AlgorithmTimestamp)
+	_, err := db.Exec("INSERT INTO simulator_events (run_name, queue_duration, alg_timestamp, tag) VALUES ($1, $2, $3, $4)",
+		event.RunName, event.QueueDuration, event.AlgorithmTimestamp, event.Tag)
 	if err != nil {
 		return err
 	}
@@ -21,14 +22,15 @@ func InsertSimulatorEvent(db *sql.DB, event SimulatorEvent) error {
 }
 
 func GetSimulatorEvents(db *sql.DB, runName string) ([]SimulatorEvent, error) {
-	rows, err := db.Query("SELECT (run_name, queue_duration, alg_timestamp) FROM simulator_events WHERE run_name = $1", runName)
+	rows, err := db.Query("SELECT * FROM simulator_events WHERE run_name = $1", runName)
 	if err != nil {
 		return nil, err
 	}
 	var events []SimulatorEvent
 	for rows.Next() {
 		var event SimulatorEvent
-		err = rows.Scan(&event.RunName, &event.QueueDuration, &event.AlgorithmTimestamp)
+		var id int
+		err = rows.Scan(&id, &event.RunName, &event.QueueDuration, &event.AlgorithmTimestamp, &event.Tag)
 		if err != nil {
 			return nil, err
 		}
